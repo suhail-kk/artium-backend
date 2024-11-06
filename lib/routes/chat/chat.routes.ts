9fafe9e7cf964955ca2f73a61dbeabcdaa5ce775
imthiyaz <imthiyaz@anjamerica.com>
2024-11-06 12:21:47 +0530

    fix: multer remove

--- lib/routes/chat/chat.routes.ts
+++ lib/routes/chat/chat.routes.ts
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
-import { upload } from "@/lib/utils/storage.utils";
-import {createChat,updateChat,listConversations,listMessages,updateOfferStatus, markAllRead, getParticipant,checkConversationExist} from '@/lib/controllers/chat/chat.controller'
 
-router.post("/create", createChat)
+import {
+	createChat,
+	updateChat,
+	listConversations,
+	listMessages,
+	updateOfferStatus,
+	markAllRead,
+	getParticipant,
+	checkConversationExist,
+} from '@/lib/controllers/chat/chat.controller';
 
-router.get('/listChats',listConversations)
-router.get('/listMessages',listMessages)
-router.put('/update/:id',updateChat)
-router.put('/offer/update',updateOfferStatus)
-router.put("/readAllMessages",markAllRead)
-router.post('/getParticipant',checkConversationExist)
-router.post('/checkConversation',checkConversationExist)
-export { router };      
+router.post('/create', createChat);
+
+router.get('/listChats', listConversations);
+router.get('/listMessages', listMessages);
+router.put('/update/:id', updateChat);
+router.put('/offer/update', updateOfferStatus);
+router.put('/readAllMessages', markAllRead);
+router.post('/getParticipant', checkConversationExist);
+router.post('/checkConversation', checkConversationExist);
+export { router };
